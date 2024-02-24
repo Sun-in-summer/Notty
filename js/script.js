8580c2e8588f9951b92f0d1b3c35d1fdf98d71fd
Elena <eamatytsina@gmail.com>
2024-02-24 22:56:39 +0300

    menu appearing added

--- js/script.js
+++ js/script.js
@@ -13,6 +13,24 @@ window.addEventListener('load', function () {
   new Ukiyo(images);
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu__active');
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu__active');
+    });
+  });
+});
+
 function elemInViewport(elem, full) {
   var box = elem.getBoundingClientRect();
   var top = box.top;
